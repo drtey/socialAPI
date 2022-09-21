6ead139506f9b545ca5214e628328447147de1cd
Drtey <javiggv99@gmail.com>
2022-09-21 19:55:41 +0200

    mysql

--- .history/app/database_20220921194638.py
+++ .history/app/database_20220921194638.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-SQLALCHEMY_DATABASE_URL = 'mysql+pymysql://root@localhost:3306/api'
+SQLALCHEMY_DATABASE_URL = 'mysql+pymysql://user@localhost:3306/api'
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 
