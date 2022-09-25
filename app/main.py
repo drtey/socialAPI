a18a7426277d9c34c38b1d2c7e3140c18eb9aecf
m33i <shionish@gmail.com>
2022-09-25 19:57:15 +0200

    fix vote added to routers

--- app/main.py
+++ app/main.py
@@ -2,13 +2,14 @@ from fastapi import FastAPI
 
 from . import models
 from .database import engine
-from .routers import post, user, auth
+from .routers import post, user, auth, vote
 from .config import settings
 
 models.Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
         
+app.include_router(vote.router)
 app.include_router(post.router)      
 app.include_router(user.router)      
 app.include_router(auth.router)      
